617d1f2319779c8698c4839abf8f6604eceb5fcb
HoaNe <146044211+HoaNeee@users.noreply.github.com>
2025-06-09 17:41:19 +0700

    change url

--- src/utils/requets.js
+++ src/utils/requets.js
@@ -1,4 +1,4 @@
-export const BASE_URL = `http://192.168.1.183:3001`;
+export const BASE_URL = `https://basic-be-todolist.onrender.com`;
 const API_URL = `${BASE_URL}/api/v1`;
 
 export const get = async (path, token) => {
